9b5a5801ec9db734303ae65acc7b78a217e2f65c
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2025-12-01 22:30:19 +0000

    feat: initialize frontend-mentor-solutions project with Angular and SSR setup.

---
+++ frontend-mentor-solutions/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { SocialLinksProfile } from './social-links-profile/social-links-profile';
+
+export const routes: Routes = [
+  { path: '', component: SocialLinksProfile },
+  { path: '**', redirectTo: '' }
+];
